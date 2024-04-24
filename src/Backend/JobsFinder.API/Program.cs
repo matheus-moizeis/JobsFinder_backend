8f436a118ded67a87d6eb77767866b18f0419c78
Matheus Moizeis <matheusmozeis@gmail.com>
2024-04-23 23:48:53 -0300

    feat: customizando um converter para eliminar espaços em branco

--- src/Backend/JobsFinder.API/Program.cs
+++ src/Backend/JobsFinder.API/Program.cs
@@ -1,3 +1,4 @@
+using JobsFinder.API.Converters;
 using JobsFinder.API.Filters;
 using JobsFinder.Application;
 using JobsFinder.Infrastructure;
@@ -8,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new StringConverter()));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
