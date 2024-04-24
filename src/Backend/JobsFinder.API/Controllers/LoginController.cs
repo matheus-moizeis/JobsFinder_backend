407a8a5e549e27acc06c71becf138204dd86a60a
Matheus Moizeis <matheusmozeis@gmail.com>
2024-04-24 00:27:58 -0300

    feat: adicionando endpoint para login

---
+++ src/Backend/JobsFinder.API/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using JobsFinder.Application.UseCases.Login.DoLogin;
+using JobsFinder.Communication.Requests;
+using JobsFinder.Communication.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobsFinder.API.Controllers;
+
+public class LoginController : JobsFinderController
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegistredUserJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> Login(
+        [FromServices] IDoLoginUseCase useCase,
+        [FromBody] RequestLoginJson request
+        )
+    {
+        var response = await useCase.Execute(request);
+
+        return Ok(response);
+    }
+}
