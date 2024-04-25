7f61d8eea66df249e95f2b2215e9b85c2bec2576
Matheus Moizeis <matheusmozeis@gmail.com>
2024-04-24 22:42:19 -0300

    feat: configurando o swagger para incluir tokens nas requisiões.

--- src/Backend/JobsFinder.API/Program.cs
+++ src/Backend/JobsFinder.API/Program.cs
@@ -4,6 +4,8 @@ using JobsFinder.Application;
 using JobsFinder.Infrastructure;
 using JobsFinder.Infrastructure.Extensions;
 using JobsFinder.Infrastructure.Migrations;
+using Microsoft.OpenApi.Models;
+using System.Security.Cryptography.Xml;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +14,37 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new StringConverter()));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme.
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Example: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                       Type = ReferenceType.SecurityScheme,
+                       Id = "Bearer"
+                },
+                Scheme = "oath2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 //builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
 
